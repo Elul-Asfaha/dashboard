cc82fae041315f39fac0534d564e5e84a0fbb224
elul asfaha <scorpiondota22@gmail.com>
2023-08-31 12:33:40 +0300

    added top bar fixed usememo issue with theme

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,53 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlined from "@mui/icons-material/DarkModeOutlined";
+import NotificationsModeOutlined from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlined from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
 const Topbar = () => {
-    return <div>Topbar</div>;
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+    return (
+        <Box display='flex' justifyContent='space-between' p={2}>
+            <Box
+                display='flex'
+                backgroundColor={colors.primary[400]}
+                borderRadius='3px'
+            >
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder='Search' />
+                <IconButton type='button' sx={{ p: 1 }}>
+                    <SearchIcon />
+                </IconButton>
+            </Box>
+            <Box display='flex'>
+                <IconButton
+                    onClick={colorMode.toggleColorMode}
+                    sx={{ display: "flex" }}
+                >
+                    {theme.palette.mode === "dark" ? (
+                        <DarkModeOutlined />
+                    ) : (
+                        <LightModeOutlined />
+                    )}
+                </IconButton>
+                <IconButton sx={{ display: "flex" }}>
+                    <NotificationsModeOutlined />
+                </IconButton>
+                <IconButton sx={{ display: "flex" }}>
+                    <SettingsOutlinedIcon />
+                </IconButton>
+                <IconButton sx={{ display: "flex" }}>
+                    <PersonOutlined />
+                </IconButton>
+            </Box>
+        </Box>
+    );
 };
 
 export default Topbar;
--- src/theme.jsx
+++ src/theme.jsx
@@ -160,30 +160,30 @@ export const themeSettings = (mode) => {
                   }),
         },
         typography: {
-            fontFamily: ["Source Code pro", "sabs-serif"].join(","),
+            fontFamily: ["Source Code pro", "sans-serif"].join(","),
             fontSize: 12,
             h1: {
-                fontFamily: ["Source Code pro", "sabs-serif"].join(","),
+                fontFamily: ["Source Code pro", "sans-serif"].join(","),
                 fontSize: 40,
             },
             h2: {
-                fontFamily: ["Source Code pro", "sabs-serif"].join(","),
+                fontFamily: ["Source Code pro", "sans-serif"].join(","),
                 fontSize: 32,
             },
             h3: {
-                fontFamily: ["Source Code pro", "sabs-serif"].join(","),
+                fontFamily: ["Source Code pro", "sans-serif"].join(","),
                 fontSize: 24,
             },
             h4: {
-                fontFamily: ["Source Code pro", "sabs-serif"].join(","),
+                fontFamily: ["Source Code pro", "sans-serif"].join(","),
                 fontSize: 20,
             },
             h5: {
-                fontFamily: ["Source Code pro", "sabs-serif"].join(","),
+                fontFamily: ["Source Code pro", "sans-serif"].join(","),
                 fontSize: 16,
             },
             h6: {
-                fontFamily: ["Source Code pro", "sabs-serif"].join(","),
+                fontFamily: ["Source Code pro", "sans-serif"].join(","),
                 fontSize: 14,
             },
         },
@@ -196,10 +196,13 @@ export const ColorModeContext = createContext({
 });
 export const useMode = () => {
     const [mode, setMode] = useState("dark");
-    const colorMode = useMemo(() => {
-        toggleColorMode: () =>
-            setMode((prev) => (prev === "light" ? "dark" : "light"));
-    }, []);
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () =>
+                setMode((prev) => (prev === "light" ? "dark" : "light")),
+        }),
+        []
+    );
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
     return [theme, colorMode];
 };
