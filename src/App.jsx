5b65bd834334b195772d4e5f15ad34457db1f20a
elul asfaha <scorpiondota22@gmail.com>
2023-08-31 15:24:26 +0300

    added routes for future use working on sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import { Route, Routes } from "react-router-dom";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import Faq from "./scenes/faq";
+// import Geography from "./scenes/Geography";
+// import Calendar from "./scenes/calendar";
+
 function App() {
     const [theme, colorMode] = useMode();
     return (
@@ -10,6 +24,19 @@ function App() {
                 <div className='app'>
                     <main className='content'>
                         <Topbar />
+                        <Routes>
+                            <Route path='/' element={<Dashboard />} />
+                            {/* <Route path='/team' element={<Team />} />
+                        <Route path='/invoices' element={<Invoices />} />
+                        <Route path='/contacts' element={<Contacts />} />
+                        <Route path='/bar' element={<Bar />} />
+                        <Route path='/form' element={<Form />} />
+                        <Route path='/line' element={<Line />} />
+                        <Route path='/pie' element={<Pie />} />
+                        <Route path='/faq' element={<Faq />} />
+                        <Route path='/geography' element={<Geography />} />
+                        <Route path='/Calendar' element={<Calendar />} /> */}
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,80 @@
+import { useState } from "react";
+import user from "../../assets/user.png";
+import {
+    Sidebar as ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+} from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
+    return (
+        <Box
+            sx={{
+                " &. pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`,
+                },
+                " &. pro-icon-wrapper": {
+                    background: `transparent !important`,
+                },
+                " &. pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important",
+                },
+                " &. pro-inner-item:hover": {
+                    color: "#868dfb !important",
+                },
+                " &. pro-menu-item.active": {
+                    color: "#6870fa !important",
+                },
+            }}
+        >
+            {!isCollapsed && (
+                <Box mb='25px'>
+                    <Box
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
+                    >
+                        <img
+                            alt='profile-user'
+                            width='100px'
+                            height='100px'
+                            src={user}
+                            style={{ cursor: "pointer", borderRadius: "50%" }}
+                        />
+                    </Box>
+                    <Box textAlign='center'>
+                        <Typography
+                            variant='h2'
+                            colr={colors.grey[100]}
+                            fontWeight='bold'
+                            sx={{ m: "10px 0 0 0" }}
+                        >
+                            Elul Asfaha
+                        </Typography>
+                        <Typography>Vp Fancy Admin</Typography>
+                    </Box>
+                </Box>
+            )}
+        </Box>
+    );
 };
 
 export default Sidebar;
