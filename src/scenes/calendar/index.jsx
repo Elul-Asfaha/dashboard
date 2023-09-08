0919b7e50833e1610e014ea316d95cd672427427
elul asfaha <scorpiondota22@gmail.com>
2023-09-08 17:33:50 +0300

    fixed some bugs, added the calendar display

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -24,7 +24,7 @@ const Calendar = () => {
         const calendarApi = selected.view.calendar;
         calendarApi.unselect();
         if (title) {
-            calendarApi.addevent({
+            calendarApi.addEvent({
                 id: `${selected.dateStr}-${title}`,
                 title,
                 start: selected.startStr,
@@ -71,20 +71,54 @@ const Calendar = () => {
                                     primary={event.title}
                                     secondary={
                                         <Typography>
-                                            {
-                                                (formatDate(event.start),
-                                                {
-                                                    year: "numeric",
-                                                    month: "short",
-                                                    day: "numeric",
-                                                })
-                                            }
+                                            {formatDate(event.start, {
+                                                year: "numeric",
+                                                month: "short",
+                                                day: "numeric",
+                                            })}
                                         </Typography>
                                     }
                                 />
                             </ListItem>
                         ))}
                     </List>
+                </Box>
+                {/* CALENDAR */}
+                <Box flex='1 1 100%' ml='15px'>
+                    <FullCalendar
+                        height='75vh'
+                        plugins={[
+                            dayGridPlugin,
+                            timeGridPlugin,
+                            interactionPlugin,
+                            listPlugin,
+                        ]}
+                        headerToolbar={{
+                            left: "prev,next,today",
+                            center: "title",
+                            right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+                        }}
+                        initialView='dayGridMonth'
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        select={handleDateClick}
+                        eventClick={handleEventClick}
+                        eventsSet={(events) => setCurrentEvents(events)}
+                        initialEvents={[
+                            {
+                                id: "1234",
+                                title: "All-day event",
+                                date: "2023-09-08",
+                            },
+                            {
+                                id: "4321",
+                                title: "Timed event",
+                                date: "2023-09-12",
+                            },
+                        ]}
+                    />
                 </Box>
             </Box>
         </Box>
