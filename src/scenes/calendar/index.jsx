78a2790daeb713aeb37a41c5b6e0d1349a43b6dc
elul asfaha <scorpiondota22@gmail.com>
2023-09-08 16:37:48 +0300

    working on calendar

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+    useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+const Calendar = () => {
+    const theme = useTheme();
+    const colors = token(theme.palette.mode);
+    const [currentEvents, setCurrentEvents] = useState([]);
+    const handleDateClick = (selected) => {
+        const title = prompt("Please enter a new title for your event");
+        const calendarApi = selected.view.calendar;
+        calendarApi.unselect();
+    };
+    return <Box></Box>;
+};
+export default Calendar;
