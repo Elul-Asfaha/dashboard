fa582f352c1bf2e75622b92d0f58fc9a50f09e2e
elul asfaha <scorpiondota22@gmail.com>
2023-09-08 15:26:42 +0300

    adding a form validation

---
+++ src/scenes/form/index.js
@@ -0,0 +1,40 @@
+import { Box, Button, TextField } from "@mui/material"
+import { Formik } from "formik"
+import * as yup from "yup"
+import useMediaQuery from "@mui/material"
+import Header from "../../components/Header"
+const Form = () => {
+    const isNonMobile = useMediaQuery("(min-width:600px)")
+    const handleFormSubmit = (value) => {
+        console.log(value)
+    }
+    const initialValues = {
+        firtName: "",
+        lastName: "",
+        email: "",
+        contact: "",
+        address1: "",
+        address2: "",
+    }
+    const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/
+    const userSchema = yup.object().shape({
+        firstName: yup.string().required("required"),
+        lastName: yup.string().required("required"),
+        email: yup.string().email("invalid email").required("required"),
+        contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("required"),
+        address1: yup.string().required("required"),
+        address2: yup.string().required("required"),
+    })
+    return (
+        <Box m="20px">
+            <Header title="CREATE USER" subtitle="Create a New User profile" />
+            <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={initialValues}
+                validationSchema={userSchema}
+            ></Formik>
+        </Box>
+    )
+}
+
+export default Form
