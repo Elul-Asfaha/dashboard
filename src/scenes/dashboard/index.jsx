f0993315ecbcd46ffa8cc1a1f50fbf968b2cfbf5
elul asfaha <scorpiondota22@gmail.com>
2023-09-12 23:37:47 +0300

    working on dashboard progress

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, IconButton, Typography, useTheme, Button } from "@mui/material";
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import GeoChart from "../../components/GeoChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 
 const Dashboard = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     return (
         <Box m='20px'>
             <Box
@@ -13,6 +27,20 @@ const Dashboard = () => {
                     title='DASHBOARD'
                     subtitle='Welcome to your dashboard'
                 />
+            </Box>
+            <Box>
+                <Button
+                    sx={{
+                        backgroundColor: colors.blueAccent[700],
+                        color: colors.grey[100],
+                        fontSize: "14px",
+                        fontWeight: "bold",
+                        padding: "10px 20px",
+                    }}
+                >
+                    <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+                    DOWNLOAD REPORTS
+                </Button>
             </Box>
         </Box>
     );
