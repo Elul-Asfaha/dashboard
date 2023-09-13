ba0c459f07f0549b6c8ec5d4ff31b39e27e0e778
elul asfaha <scorpiondota22@gmail.com>
2023-09-13 15:49:17 +0300

    fixed an issue with statbox ui alignments adding content to dashboard

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -20,23 +20,24 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
                 <Box>
                     <ProgressCircle />
                 </Box>
-                <Box display='flex' justifyContent='space-between'>
-                    <Typography
-                        variant='h5'
-                        fontWeight='bold'
-                        sx={{ color: colors.greenAccent[500] }}
-                    >
-                        {subtitle}
-                    </Typography>
+            </Box>
 
-                    <Typography
-                        variant='h5'
-                        fontWeight='italic'
-                        sx={{ color: colors.greenAccent[600] }}
-                    >
-                        {increase}
-                    </Typography>
-                </Box>
+            <Box display='flex' justifyContent='space-between'>
+                <Typography
+                    variant='h5'
+                    fontWeight='bold'
+                    sx={{ color: colors.greenAccent[500] }}
+                >
+                    {subtitle}
+                </Typography>
+
+                <Typography
+                    variant='h5'
+                    fontWeight='italic'
+                    sx={{ color: colors.greenAccent[600] }}
+                >
+                    {increase}
+                </Typography>
             </Box>
         </Box>
     );
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -27,20 +27,162 @@ const Dashboard = () => {
                     title='DASHBOARD'
                     subtitle='Welcome to your dashboard'
                 />
+                <Box>
+                    <Button
+                        sx={{
+                            backgroundColor: colors.blueAccent[700],
+                            color: colors.grey[100],
+                            fontSize: "14px",
+                            fontWeight: "bold",
+                            padding: "10px 20px",
+                        }}
+                    >
+                        <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+                        DOWNLOAD REPORTS
+                    </Button>
+                </Box>
             </Box>
-            <Box>
-                <Button
-                    sx={{
-                        backgroundColor: colors.blueAccent[700],
-                        color: colors.grey[100],
-                        fontSize: "14px",
-                        fontWeight: "bold",
-                        padding: "10px 20px",
-                    }}
+
+            {/*GRID & CHARTS*/}
+            <Box
+                display='grid'
+                gridTemplateColumns='repeat(12,1fr)'
+                gridAutoRows='140px'
+                gap='20px'
+            >
+                {/*row 1*/}
+                <Box
+                    gridColumn='span 3'
+                    backgroundColor={colors.primary[400]}
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
                 >
-                    <DownloadOutlinedIcon sx={{ mr: "10px" }} />
-                    DOWNLOAD REPORTS
-                </Button>
+                    <StatBox
+                        title='12,361'
+                        subtitle='Emails Sent'
+                        progress='0.75'
+                        increase='+14%'
+                        icon={
+                            <EmailIcon
+                                sx={{
+                                    color: colors.greenAccent[600],
+                                    fontSize: "26px",
+                                }}
+                            />
+                        }
+                    />
+                </Box>
+                <Box
+                    gridColumn='span 3'
+                    backgroundColor={colors.primary[400]}
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
+                >
+                    <StatBox
+                        title='431,225'
+                        subtitle='Sales Obtained'
+                        progress='0.5'
+                        increase='+21%'
+                        icon={
+                            <PointOfSaleIcon
+                                sx={{
+                                    color: colors.greenAccent[600],
+                                    fontSize: "26px",
+                                }}
+                            />
+                        }
+                    />
+                </Box>
+                <Box
+                    gridColumn='span 3'
+                    backgroundColor={colors.primary[400]}
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
+                >
+                    <StatBox
+                        title='12,361'
+                        subtitle='New Clients'
+                        progress='0.3'
+                        increase='+5%'
+                        icon={
+                            <PersonAddIcon
+                                sx={{
+                                    color: colors.greenAccent[600],
+                                    fontSize: "26px",
+                                }}
+                            />
+                        }
+                    />
+                </Box>
+                <Box
+                    gridColumn='span 3'
+                    backgroundColor={colors.primary[400]}
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
+                >
+                    <StatBox
+                        title='1,325,134'
+                        subtitle='Traffice Inbound'
+                        progress='0.8'
+                        increase='+43%'
+                        icon={
+                            <TrafficIcon
+                                sx={{
+                                    color: colors.greenAccent[600],
+                                    fontSize: "26px",
+                                }}
+                            />
+                        }
+                    />
+                </Box>
+                {/*Row Two */}
+                <Box
+                    gridColumn='span 8'
+                    gridRow='span 2'
+                    backgroundColor={colors.primary[400]}
+                >
+                    <Box
+                        mt='25px'
+                        p=' 0 30px'
+                        display='flex'
+                        justifyContent='space-between'
+                        alignItems='center'
+                    >
+                        <Box>
+                            <Typography
+                                variant='h5'
+                                fontWeight='600'
+                                color={colors.grey[100]}
+                            >
+                                Revenue Generated
+                            </Typography>
+                            <Typography
+                                variant='h3'
+                                fontWeight='500'
+                                color={colors.greenAccent[500]}
+                            >
+                                $5,934,232
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <IconButton>
+                                <DownloadOutlinedIcon
+                                    sx={{
+                                        fontSize: "26px",
+                                        color: colors.greenAccent[500],
+                                    }}
+                                />
+                            </IconButton>
+                        </Box>
+                    </Box>
+                    <Box height='250px'>
+                        <LineChart isDashBoard={true} />
+                    </Box>
+                </Box>
             </Box>
         </Box>
     );
